60d64b4097b0bf5709cb9f5574ce782fbfb40774
VazoMonitor <vazomonitoramento@gmail.com>
2025-02-21 14:32:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const cors = require('cors');
 const apiRoutes = require('./routes/api');
 const WebSocket = require('ws');
 const MonitoringService = require('./services/monitoringService');
+const https = require('https');
+const fs = require('fs');
+
+// Carregar os certificados SSL gerados pelo Certbot
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.vazo.info/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api.vazo.info/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/api.vazo.info/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8880; // Alterando para usar a porta 8880
 
 app.use(cors());
 app.use(express.json());
@@ -30,12 +43,19 @@ wss.on('connection', (ws) => {
 
 MonitoringService.init();
 
-const server = app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+// Criar o servidor HTTPS
+const server = https.createServer(credentials, app, () => {
+  console.log(`Servidor HTTPS rodando na porta ${port}`);
 });
 
+// Conectar WebSocket
 server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
     wss.emit('connection', ws, request);
   });
-}); 
+});
+
+// Iniciar o servidor
+server.listen(port, () => {
+  console.log(`Servidor rodando com HTTPS na porta ${port}`);
+});
